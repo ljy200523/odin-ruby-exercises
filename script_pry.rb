b7e12db4682e21913b4dba097fe7a06f8572a901
Ian Lam <ljy200523@gmail.com>
2026-04-01 15:47:07 +0800

    Include rdbg debugger extension

--- script_pry.rb
+++ script_pry.rb
@@ -1,10 +1,9 @@
-require 'pry-byebug'
 
 def isogram?(string)
   original_length = string.length
   string_array = string.downcase.split
 
-  binding.pry
+  debugger
 
   unique_length = string_array.uniq.length
   original_length == unique_length
